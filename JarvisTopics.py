36dd94d6240ff0ba50f838bd550d0a1350a61c5f
A.Taylor <ashtrix2k@googlemail.com>
2012-09-21 16:26:10 +0100

    Jarvis Topics class file.
    This class file stores all the topics that jarvis may ask the user.

---
+++ JarvisTopics.py
@@ -0,0 +1,14 @@
+"""
+- Class Name : Jarvis Topics Class
+- Class description : Class file that stores the topics that Jarvis will use.
+- Language : Python
+- Coded by : A.Taylor
+- Coder Name : S3pHiRoTh
+
+"""
+
+class Jarvis_Topics(object) :
+	""" Jarvis Topics """
+	def General(self,question, answer) :
+		self.question = quesion
+		self.answer = answer
