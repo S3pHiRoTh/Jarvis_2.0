d0f243723f21a933160ed5d3d3c60bd6cc9f0721
A.Taylor <ashtrix2k@googlemail.com>
2012-09-21 16:25:35 +0100

    Jarvis Random Question Class
    This class stores the random questions that Jarvis may ask the user if
    the chat is getting a little repeatitive.

---
+++ JarvisRandomQuestion.py
@@ -0,0 +1,16 @@
+"""
+- Class Name : Jarvis Random question class file
+- Class description : Thos class file calls the questions that jarvis may ask the user.
+- Language : Python
+- Coded by : A.Taylor
+- Coder Name : S3pHiRoTh
+
+"""
+# Import framework
+
+import random
+from random import choice
+
+def Random_quest() :
+	None
+	# This class file is nowhere being finished, it currently stands as an empty class file.
