f337196b37172d43b19f6f4dd41a09225cad9b5b
A.Taylor <ashtrix2k@googlemail.com>
2012-09-21 16:24:46 +0100

    Jarvis MAIN
    Main Jarvis file.

---
+++ Jarvis.py
@@ -0,0 +1,21 @@
+"""
+- Program Name : Jarvis Bot [2.0]
+- Program description : An AI chatbot.
+- Language : Python
+- Coded by : A.Taylor
+- Coder Name : S3pHiRoTh
+
+"""
+
+# Any framework that needs importing goes here.
+from random import choice
+
+# Global variables
+# Jarvis variables
+RESPONSES = ['Y', 'N']
+
+# User Variables
+un = raw_input("Type your Name : ")
+
+# Import Independant Jarvis classes 
+import JarvisTopics, JarvisRandomQuestion
