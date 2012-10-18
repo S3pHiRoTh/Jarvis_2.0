2b8d1ed6fe142f9e5cd2c160459895b3e40b282d
A.Taylor <ashtrix2k@googlemail.com>
2012-10-18 18:31:50 +0100

    Updated JarvisRandomQuestion, JarvisTopics Class
    Changelog :
    - Organized code
    - Created Class File to organize variables.

--- JarvisRandomQuestion.py
+++ JarvisRandomQuestion.py
@@ -7,25 +7,22 @@
 
 """
 # Import framework
-
 import random
 from random import choice
-import Jarvis
 # Possible inputs for the random question
 
-def Random_quest() :
-	Rand_question_ReplyTRUE = ("y", "yeah", "yes", "yep", "sure", "alright", "of course", "go ahead")
-	Rand_question_ReplyFALSE = ("n", "no", "you cant", "nope")
-	Rand_question = raw_input("%s%s Hey %s i want to ask you a question, but it can be about anything! Can i ? : " %(BOT_NAME[0], BOT_MISC, un))
-	if (str(Rand_question.lower())) in Rand_question_ReplyTRUE :
-		print "%s%s Ok %s thanks! I will ask you a totally random question! \n" %(BOT_NAME[0], BOT_MISC, un)
-		Random_questionINIT()
-	elif (str(Rand_question.lower())) in Rand_question_ReplyFALSE :
-		print "%s%s Aw, i had a super random question to ask you %s! \n" %(BOT_NAME[0], BOT_MISC, un)
-		return Jarvis.UserResponse()
-	else :
-		"%s%s Erm, i didn't recognize that response! Please try again %s! " %(BOT_NAME[0], BOT_MISC, un)
-	
-def Random_questionINIT() :
-	None
+class JarvisRandomQuestion(object) :
+	""" Class for Jarvis to ask the user random questions. """
+	def Random_quest(self) :
+		Rand_question_ReplyTRUE = ("y", "yeah", "yes", "yep", "sure", "alright", "of course", "go ahead")
+		Rand_question_ReplyFALSE = ("n", "no", "you cant", "nope")
+		Rand_question = raw_input("Hey i want to ask you a question, but it can be about anything! Can i ? : ")
+		if (str(Rand_question.lower())) in Rand_question_ReplyTRUE :
+			print "%s%s Ok thanks, %s! I will ask you a totally random question! \n" %(Jarvis.JarvisMainVar.BOT_NAME[0], Jarvis.JarvisMainVar.MISC_BOT, Jarvis.JarvisMainVar.USER_NAME[0])
+		elif (str(Rand_question.lower())) in Rand_question_ReplyFALSE :
+			print "%s%s Aw, i had a super random question to ask you %s! \n" %(Jarvis.JarvisMainVar.BOT_NAME[0], Jarvis.JarvisMainVar.MISC_BOT, Jarvis.JarvisMainVar.USER_NAME[0])
+			return Jarvis.UserResponse()
+		else :
+			"%s%s Erm, i didn't recognize that response! Please try again %s! " %(Jarvis.JarvisMainVar.BOT_NAME[0], Jarvis.JarvisMainVar.MISC_BOT, Jarvis.JarvisMainVar.USER_NAME[0])
+		
 	
