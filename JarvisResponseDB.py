172eb512c1b3a9a6d6d3154fed7e8662b4bed46a
A.Taylor <ashtrix2k@googlemail.com>
2012-10-20 02:45:36 +0100

    JarvisResponseDB Class
    Changelog :
    - Started working on the responses.
    * Not integrated them into the main Jarvis Src though.

--- JarvisResponseDB.py
+++ JarvisResponseDB.py
@@ -1,5 +1,5 @@
 """
-- Class Name : Jarvis Bot [2.0 [JarvisResponseDB]
+- Class Name : Jarvis Bot [2.0] [JarvisResponseDB]
 - Class description : Data file that stores all the responses.
 - Language : Python
 - Coded by : A.Taylor
@@ -7,3 +7,41 @@
 
 """
 
+class JarvisGreeting(object) :
+	""" Jarvis Greeting responses class """
+	
+	# English Greeting
+	Greeting_EN = "hello"
+	Greeting_EN1 = "hi"
+	Greeting_EN2 = "howdy"
+		
+	# Spanish Greeting
+	Greeting_SPA = "hola"
+		
+	# German Greeting
+	Greeting_GER = "hallo"
+		
+	# French Greeting
+	Greeting_FRE = "bonjour"
+		
+	# Romanian Greeting
+	Greeting_RO = "sal"
+		
+	# Islamic Greeting
+	Greeting_ISL = "salaam"
+		
+class JarvisGoodbye(object) :
+	""" Possible Bye keywords for Jarvis """
+		
+	# English Goodbye Diag
+	Goodbye_EN = "goodbye"
+	Goodbye_EN1 = "bye"
+	Goodbye_EN2 = "cya"
+		
+	# Spanish Goodbye Diag
+	Goodbye_SPA = "Adios"
+		
+	# French Goodbye Diag
+	Goodbye_FRE = "au revoir"
+	
+	
